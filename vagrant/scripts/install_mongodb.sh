ac568ea21523476dffa4b624011cd33bd85f0bd4
BowgartField <jerichez02@gmail.com>
2021-12-21 19:33:17 +0100

    Correct mongodv service in vagrant

--- vagrant/scripts/install_mongodb.sh
+++ vagrant/scripts/install_mongodb.sh
@@ -3,3 +3,6 @@ sudo wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 sudo apt-get update
 sudo apt install -y mongodb-org
+
+# Start mongodb service
+sudo systemctl start mongod
